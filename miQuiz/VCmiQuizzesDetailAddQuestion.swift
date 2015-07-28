384282a6c037a10393702ca5da245f756beea68d
jayorjason50 <jasonspence@Jasons-Mac-mini.home>
2015-07-28 22:05:32 +0100

    first push

---
+++ miQuiz/VCmiQuizzesDetailAddQuestion.swift
@@ -0,0 +1,42 @@
+//
+//  VCmiQuizzesDetailAddQuestion.swift
+//  miQuiz
+//
+//  Created by Jason Spence on 27/07/2015.
+//  Copyright (c) 2015 Jason Spence. All rights reserved.
+//
+
+import UIKit
+
+class VCmiQuizzesDetailAddQuestion: UIViewController {
+
+ 
+    
+
+    @IBAction func close(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- miQuiz/ViewController.swift
+++ miQuiz/ViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    @IBAction func toggleLoginView(sender: AnyObject) {
+        var toggle = LoginView.viewWithTag(0)?.hidden
+        if toggle == true{
+            LoginView.viewWithTag(0)?.hidden = false}
+        else {LoginView.viewWithTag(0)?.hidden = true}
+    }
+   
+    @IBOutlet weak var LoginView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
